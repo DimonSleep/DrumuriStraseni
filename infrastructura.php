d694f2adf9134c732164a5caeb2ba556df9d8e64
Dumitru Andoni <62153515+DimonSleep@users.noreply.github.com>
2022-01-30 17:38:06 +0200

    modd2

--- infrastructura.php
+++ infrastructura.php
@@ -1,177 +1,37 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-	<title>Drumuri Străşeni </title>
+	<title>Drumuri Strășeni</title>
 	<link rel="shortcut icon" href="img/favicon.png" />
-	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
-
-	<!-- Bootstrap -->
 	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
-
-	<!-- Owl Carousel -->
 	<link type="text/css" rel="stylesheet" href="css/owl.carousel.css" />
 	<link type="text/css" rel="stylesheet" href="css/owl.theme.default.css" />
-
-	<!-- Magnific Popup -->
-	<!--<link type="text/css" rel="stylesheet" href="css/magnific-popup.css" />-->
-
-	<!-- Font Awesome Icon -->
 	<link rel="stylesheet" href="css/font-awesome.min.css">
-
-	<!-- Custom stlylesheet -->
 	<link type="text/css" rel="stylesheet" href="css/style.css" />
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 </head>
-
 <body>
-
 	<!-- Header -->
 	<header>
-
 		<!-- Nav -->
-  <?php
+		<?php
    include('./php/nav.php');
    ?>
 		<!-- /Nav -->
-
 		<!-- header wrapper -->
 		<div class="header-wrapper sm-padding bg-grey">
 			<div >
-				<center><h2>Infrastructură Drumuri.</h2></center>
-
-
-                <div id="blog" class="section md-padding">
-
-                    <!-- Container -->
-                    <div class="container">
-            
-            
-                                    <div class="container">
-            <center>
-                                        <h3>S.A.”Drumuri Străşeni” </h3>
-										<p>
-											
-										</p>
-                                    </div>
-
+				<center><h2>Organigrama</h2></center>
 			</div>
 		</div>
 		<!-- /header wrapper -->
-
 	</header>
 	<!-- /Header -->
-
 	<!-- Blog -->
-
-
-
-	
-<!-- Contact -->
-<div id="contact" class="section md-padding">
-
-	<!-- Container -->
-	<div class="container">
-
-		<!-- Row -->
-		<div class="row">
-
-			<!-- Section-header -->
-			<div class="section-header text-center">
-				<h2 class="title">Contacte</h2>
-			</div>
-			<!-- /Section-header -->
-
-			<!-- contact -->
-			<div class="col-sm-4">
-				<div class="contact">
-					<i class="fa fa-phone"></i>
-					<h3>Telefon</h3>
-					<p>0237-2-26-71</p>
-				</div>
-			</div>
-			<!-- /contact -->
-
-			<!-- contact -->
-			<div class="col-sm-4">
-				<div class="contact">
-					<i class="fa fa-envelope"></i>
-					<h3>Gmail</h3>
-					<p>office.drumuristraseni@gmail.com </p>
-				</div>
-			</div>
-			<!-- /contact -->
-
-			<!-- contact -->
-			<div class="col-sm-4">
-				<div class="contact">
-					<i class="fa fa-map-marker"></i>
-					<h3>Adresa</h3>
-					<p>Str.Orheiului nr.1<br> mun.Străşeni, Republica Moldova</p>
-				</div>
-			</div>
-			<!-- /contact -->
-
-
-		</div>
-		<!-- /Row -->
-
-	</div>
-	<!-- /Container -->
-
-</div>
-<!-- /Contact -->
-
-			<!-- Row -->
-			<div class="row">
-
-				<!-- Main -->
-				<main id="main" class="col-md-9">
-
-
-<!--map-->
-					<div class="map-responsive">
-					<iframe  src="https://maps.google.com/maps?q=Straseni&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-								</iframe><a href="https://www.emojilib.com"></a></div>
-<!--/map -->
-				</main>
-
-				<!-- /Main -->
-				
-
-
-														<!-- Aside -->
-														<?php
-   include('./php/aside.php');
-   ?>
-				<!-- Aside -->	
-
-			</div>
-			<!-- /Row -->
-
-		</div>
-		<!-- /Container -->
-
-	</div>
-	<!-- /Blog -->
-
-	<!-- Footer -->
-	
-	<!-- Footer -->
-	<footer id="footer" class="sm-padding bg-dark">
+	<div id="blog" class="section md-padding">
 
 		<!-- Container -->
 		<div class="container">
@@ -179,31 +39,47 @@
 			<!-- Row -->
 			<div class="row">
 
-				<div class="col-md-12">
+				<!-- Main -->
+				<main id="main" class="col-md-9">
+					<div class="blog">
+						<div class="blog-img">
+							<a href="Anexa drumurilor deservite de S. A. Drumuri-Strășeni.pdf" target="_blank"><img class="img-responsive" src="./img/INF.jpg" alt=""></a>
+						</div>
+							<div class="blog-content">
+								<!--  -->
+						</div>
+						<!-- blog tags -->
+						<div class="blog-tags">
 
-
-					<div class="footer-copyright footer-logo">
-						<a href="index.php"><img style='height: 100%; width: 100%; object-fit: contain' class="logo" src="img/logowi.png" alt="logo"></a>
-					
-
-					
-						<p>Copyright © 2021. All Created by <a href="https://www.demonickstudios.com" target="_blank">DemoNick Studios</a></p>
+						</div>
+						<!-- blog tags -->
+						<div class="blog-content">
+							<h3>Organigrama S.A. Drumuri Strășeni.</h3>
+							<p>Anexa drumurilor deservite de S. A. Drumuri-Strășeni<br><a href="Anexa drumurilor deservite de S. A. Drumuri-Strășeni.pdf" target="_blank"><i class="fa fa-file-pdf-o" style="font-size:20px;color:red"><font style="color: #008BC9;">Organigrama.pdf</font></i></a>.</p>
+						</div>
 					</div>
-				</div>
+				</main>
+				<!-- /Main -->
+					<!-- Aside -->
+					<?php
+   include('./php/aside.php');
+   ?>
+				<!-- /Aside -->
 
 			</div>
 			<!-- /Row -->
-
 		</div>
 		<!-- /Container -->
-
-	</footer>
+	</div>
+	<!-- /Blog -->
+	<!-- Footer -->
+	<?php
+	include('./php/footer.php');
+	?>
 	<!-- /Footer -->
-
 	<!-- Back to top -->
 	<div id="back-to-top"></div>
 	<!-- /Back to top -->
-
 	<!-- Preloader -->
 	<div id="preloader">
 		<div class="preloader">
@@ -214,14 +90,11 @@
 		</div>
 	</div>
 	<!-- /Preloader -->
-
 	<!-- jQuery Plugins -->
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
 	<script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
-
 </body>
-
 </html>
